683130fa0d9c08ddd3a389c354969c37b13101cf
pemrouz <p3pedram@hotmail.com>
2015-08-02 20:19:20 +0100

    update package name

--- test.js
+++ test.js
@@ -4,6 +4,12 @@ var expect = require('chai').expect
 describe('unique', function() {
   it('should do filter to unique values', function() {
     var a = [1,2,3,3,2]
-    expect(a.reduce(unique), 0).to.be.eql([1,2,3])
+    expect(a.reduce(unique, 1)).to.be.eql([1,2,3])
   })
+
+  it('should work with array of size 1', function() {
+    var a = [1]
+    expect(a.reduce(unique, 1)).to.be.eql([1])
+  })
+
 })
